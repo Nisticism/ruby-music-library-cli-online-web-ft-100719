1b8f30a1952fd9cceceb3091d5995a89da5ab70a
Hans Foster <fosterhans@gmail.com>
2019-11-12 21:42:42 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -6,9 +6,20 @@ class Genre
   
   def initialize(name)
     @name = name 
+  end
+  
+  def save 
     @@all << self 
   end
   
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(name)
+    new_song = Song.new(name)
+    @@all << new_song
+  end
   
   
 end
