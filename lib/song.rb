d25e23869e2d481ffa6e78fe1276aea50a6b6ec7
Hans Foster <fosterhans@gmail.com>
2019-11-12 21:46:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name 
     @artist = artist
   end
